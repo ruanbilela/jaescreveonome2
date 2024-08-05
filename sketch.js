ca1859b0be900031f3ab61c67e06db2f2db37a1f
ruanbilela <ruan.bilela@escola.pr.gov.br>
2024-08-05 11:15:12 -0300

    Create sketch.js
    adicionado js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+  } 
+  
+  function draw() {
+   
+   stroke("blue"); 
+   fill("red");
+  
+   // console.log(mouseIspressed);
+  
+    if (mouseIsPressed) {
+     rect(mouseX, mouseY, 20, 35);
+     }
+  }
